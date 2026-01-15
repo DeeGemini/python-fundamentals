63fd096f927a9fd35ba0ccb7264f9650cfeacfdc
DeeGemini <zodiactheegemini@gmail.com>
2026-01-15 23:44:33 +0200

    add basics.py with initial printing and variable examples

---
+++ Phase1_basics/basics.py
@@ -0,0 +1,16 @@
+# Printing
+print("Hello DeeGemini!")
+
+# Variables
+name = "DeeGemini"
+age = 30
+gender = "Female"
+height = 1.65 # in centimeters
+is_engineer = True
+
+# Dispalying Variables
+print(type(name))
+print(type(age))
+print(type(gender))
+print(type(height))
+print(type(is_engineer))
