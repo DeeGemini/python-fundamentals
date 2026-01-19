b52244a16848909fb6497a1e5b806f47a7963236
DeeGemini <zodiactheegemini@gmail.com>
2026-01-19 17:03:16 +0200

    add about_me.py with personal information and hobbies

---
+++ Phase1_basics/about_me.py
@@ -0,0 +1,9 @@
+# This line prins the title "About Me"
+print("About Me")
+
+name = "DeeGemini"
+age = 31
+height_cm = 165
+is_student = False
+hobbies = ["coding", "cookig", "gaming"]
+gender = "Female"
